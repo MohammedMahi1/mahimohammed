dcc6ca2b13594709b2ed57226c75d4f5fafeca83
MohammedMahi1 <mohammed.mahi012@gmail.com>
2024-04-22 00:26:20 +0100

    complete: Contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,22 @@
+import Button from "../components/Button/Button"
+import FieldForm from "../components/FieldForm/FieldForm"
+
+import { TbSend } from "react-icons/tb";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>vsfvs</h1>
-    </div>
+    <div className="main-page contact">
+      <h1>Contact me</h1>
+      <br />
+        <div className="contact-container">
+        <div className="group-form">
+        <FieldForm  type="text" label="Enter your name" />
+        <FieldForm  type="email" label="Enter your email" />
+        </div>
+        <FieldForm  type="textarea" label="Lets make somthing cool !!!" rows={5}/>
+        <Button className="btn-outlined btn-fill" label="Send it"/>
+        </div>
+        </div>
   )
 }
 
