0b381675124ced4503b767aa69b50045e5c12ec4
MohammedMahi1 <mohammed.mahi012@gmail.com>
2024-04-22 00:51:09 +0100

    add: icon and name

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,21 +1,46 @@
 import Button from "../components/Button/Button"
 import FieldForm from "../components/FieldForm/FieldForm"
 
-import { TbSend } from "react-icons/tb";
+// import React, { useRef } from 'react';
+// import emailjs from '@emailjs/browser';
+
 
 const Contact = () => {
+
+  // const form = React.MutableRefObject<null>;
+
+
+  // const sendEmail = (e:any) => {
+  //   e.preventDefault();
+
+  //   emailjs
+  //     .sendForm('service_9qh0meh', 'template_xp8z65n', form.current, {
+  //       publicKey: 'muCl8D333I8eTAZ1L',
+  //     })
+  //     .then(
+  //       () => {
+  //         console.log('SUCCESS!');
+  //       },
+  //       (error) => {
+  //         console.log('FAILED...', error.text);
+  //       },
+  //     );
+  // };
+
+
+
   return (
     <div className="main-page contact">
       <h1>Contact me</h1>
       <br />
-        <div className="contact-container">
+        <form className="contact-container" >
         <div className="group-form">
         <FieldForm  type="text" label="Enter your name" />
         <FieldForm  type="email" label="Enter your email" />
         </div>
         <FieldForm  type="textarea" label="Lets make somthing cool !!!" rows={5}/>
         <Button className="btn-outlined btn-fill" label="Send it"/>
-        </div>
+        </form>
         </div>
   )
 }
